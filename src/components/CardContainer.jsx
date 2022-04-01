930850a1b8bc6cc47641a6670f255f816e00c7e5
Leo Cucinell <leocucinell@gmail.com>
2022-03-31 18:06:46 -1000

    added some basic styling and the CSSTransition for the cards to appear

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,5 +1,6 @@
 import "./CardContainer.css";
 import React, {useState} from "react";
+import { CSSTransition } from "react-transition-group";
 
 const CardContainer = () => {
     const [cards, setCards] = useState([]);
@@ -9,7 +10,7 @@ const CardContainer = () => {
         <div className="CardContainer-container">
             <div className="CardContainer-form">
                  <input value={title} placeholder="title..." onChange={(e) => setTitle(e.target.value)} type="text" />
-                 <div onClick={() => {
+                 <div className="CardContainer-add-btn" onClick={() => {
                     const cardId = Math.floor(Math.random() * 999999999) //not perfect, OK for now
                     setCards([...cards, {
                         id: cardId,
@@ -23,10 +24,19 @@ const CardContainer = () => {
                 {
                     cards.map((card) => {
                         return(
-                            <div key={card.id} className="CardContainer-card">
-                                <span>{card.content}</span>
+                            <div key={card.id}>
+                                <CSSTransition
+                                    timeout={300}
+                                    classNames="note-card"
+                                    appear={true}
+                                    in={true}
+                                >
+                                    <div className="CardContainer-card">
+                                        <span>{card.content}</span>
+                                    </div>
+                                </CSSTransition>
                             </div>
-                        )
+                        );
                     })
                 }
             </div>
